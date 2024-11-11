ec08bef410fd97fa109ffa317eb3385f859e2ce8
OMuzalevska <oksana.muzalevskaja@gmail.com>
2024-11-11 23:51:43 +0100

    add geters & seters

--- src/main/java/dev/omuzalevska/meprimeraclass/Persona.java
+++ src/main/java/dev/omuzalevska/meprimeraclass/Persona.java
@@ -1,12 +1,12 @@
 package dev.omuzalevska.meprimeraclass;
 
 public class Persona {
-    public String firstName;
-    public String lastName;
-    public String idNumber;
-    public int birthYear;
-    public String birthCountry;
-    public char gender;
+    private String firstName;
+    private String lastName;
+    private String idNumber;
+    private int birthYear;
+    private String birthCountry;
+    private char gender;
 
     public Persona(String firstName, String lastName, String idNumber,
             int birthYear, String birthCountry, char gender) {
@@ -25,5 +25,53 @@ public class Persona {
         System.out.println("Birth Year: " + birthYear);
         System.out.println("Birth Country: " + birthCountry);
         System.out.println("Gender: " + gender);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public String getBirthCountry() {
+        return birthCountry;
+    }
+
+    public void setBirthCountry(String birthCountry) {
+        this.birthCountry = birthCountry;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
     }
 }
--- src/test/java/dev/omuzalevska/meprimeraclass/PersonaTest.java
+++ src/test/java/dev/omuzalevska/meprimeraclass/PersonaTest.java
@@ -9,13 +9,37 @@ public class PersonaTest {
     public void testPersonaConstructor() {
         Persona person = new Persona("John", "Das", "12345", 
                     1997, "USA", 'M');
-        assertEquals("John", person.firstName);
-        assertEquals("Das", person.lastName);
-        assertEquals("12345", person.idNumber);
-        assertEquals(1997, person.birthYear);
-        assertEquals("USA", person.birthCountry);
-        assertEquals('M', person.gender);
+        assertEquals("John", person.getFirstName());
+        assertEquals("Das", person.getLastName());
+        assertEquals("12345", person.getIdNumber());
+        assertEquals(1997, person.getBirthYear());
+        assertEquals("USA", person.getBirthCountry());
+        assertEquals('M', person.getGender());
     }
 
 
+    @Test
+    public void testGettersAndSetters() {
+        Persona person = new Persona("Juan", "Gomez", "12345678", 1998, "Spain", 'H');
+        assertEquals("Juan", person.getFirstName());
+        assertEquals("Gomez", person.getLastName());
+        assertEquals("12345678", person.getIdNumber());
+        assertEquals(1998, person.getBirthYear());
+        assertEquals("Spain", person.getBirthCountry());
+        assertEquals('H', person.getGender());
+
+         person.setFirstName("Carlos");
+        person.setLastName("Perez");
+        person.setIdNumber("98765432");
+        person.setBirthYear(2000);
+        person.setBirthCountry("Mexico");
+        person.setGender('M');
+
+        assertEquals("Carlos", person.getFirstName());
+        assertEquals("Perez", person.getLastName());
+        assertEquals("98765432", person.getIdNumber());
+        assertEquals(2000, person.getBirthYear());
+        assertEquals("Mexico", person.getBirthCountry());
+        assertEquals('M', person.getGender());
+    }
 }
